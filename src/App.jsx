83e1328d5d5886ba0f1ed1424a1bfd1fcc3f50f2
hakawa <hakawa.alves@gmail.com>
2022-02-13 22:42:48 -0300

    feat: initial commit

---
+++ src/App.jsx
@@ -0,0 +1,64 @@
+import { useState } from 'react';
+import './app.css';
+
+import { MaleMenus, FemaleMenus } from './helpers/constants';
+
+function App() {
+  const [openMenu, setOpenMenu] = useState({ menuName: '', isOpen: false });
+  const [selectCategory, selectedCategory] = useState({ pathName: '', imgPrefix: '', categoryPath: '', name: '' });
+  const [array, setArray] = useState([]);
+
+  const handleMenu = (menuName, isOpen) => {
+    setOpenMenu({ menuName, isOpen })
+  }
+
+  const handleSelectCategory = (pathName, imgPrefix, categoryPath, quantity, name ) => {
+    selectedCategory({pathName, imgPrefix, categoryPath, name});
+    handleMenu('', false);
+    setArray(Array.from(Array(quantity).keys()));
+  }
+
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div className='male-menu'>
+          <span className='menu-indicator' onClick={() => handleMenu('male-menu', true)}>{MaleMenus.menuName}</span>
+          {openMenu.menuName === 'male-menu' && openMenu.isOpen && 
+            <ul>
+              {MaleMenus.children.map((item) => (
+                <li onClick={() => handleSelectCategory(MaleMenus.pathName, MaleMenus.imgPrefix, item.path, item.value, item.name)}>{item.name}</li>
+              ))}
+            </ul>}
+        </div>
+        <span className='separator'>|</span>
+        <div className='female-menu'>
+          <span className='menu-indicator' onClick={() => handleMenu('female-menu', true)}>{FemaleMenus.menuName}</span>
+          {openMenu.menuName === 'female-menu' && openMenu.isOpen && 
+            <ul>
+              {FemaleMenus.children.map((item) => (
+                <li onClick={() => handleSelectCategory(FemaleMenus.pathName, FemaleMenus.imgPrefix, item.path, item.value, item.name)}>{item.name}</li>
+              ))}
+            </ul>}
+        </div>
+      </header>
+      {selectCategory.categoryPath && (
+        <div className='disclaimer'>
+          <h1>Essa é a categoria {selectCategory.name}</h1>
+          <span>Para utilizar só mentalizar no "T" /nome da categoria e id da vestimenta exemplo</span>
+          <span>/{selectCategory.name.toLocaleLowerCase()} 2</span>
+        </div>
+      )}
+      {selectCategory.categoryPath && <div className='content'>
+          {array.map((index) => (
+             <div className='assets-container' key={index}>
+              <img src={`../images/${selectCategory.categoryPath}/${selectCategory.pathName}/${selectCategory.imgPrefix}(${index}).jpg`} />
+              <p>Id: {index}</p>
+            </div>
+          ))}
+      </div>}
+    </div>
+  )
+}
+
+export default App
---
+++ src/helpers/constants.js
@@ -0,0 +1,145 @@
+export const MaleMenus = {
+    menuName: 'Masculino',
+    pathName: 'Male',
+    imgPrefix: 'M',
+    children: [
+        {
+            name: 'Mascaras',
+            value: 179,
+            path: 1
+        },
+        {
+            name: 'Calças',
+            value: 126,
+            path: 4
+        },
+        {
+            name: 'Mochila',
+            value: 84,
+            path: 5
+        },
+        {
+            name: 'Sapatos',
+            value: 95,
+            path: 6
+        },
+        {
+            name: 'NÃO SEI O QUE É',
+            value: 135,
+            path: 7
+        },
+        {
+            name: 'Blusa',
+            value: 164,
+            path: 8
+        },
+        {
+            name: 'PARECE SER COLETE',
+            value: 41,
+            path: 9
+        },
+        {
+            name: 'Jaqueta',
+            value: 331,
+            path: 11
+        },
+        {
+            name: 'Chapeu/P0',
+            value: 148,
+            path: 'p0'
+        },
+        {
+            name: 'Oculos/P1',
+            value: 29,
+            path: 'p1'
+        },
+        {
+            name: 'Brincos/P2',
+            value: 40,
+            path: 'p2'
+        },
+        {
+            name: 'Pulso/P6',
+            value: 39,
+            path: 'p6'
+        },
+        {
+            name: 'Pulso/P7',
+            value: 7,
+            path: 'p7'
+        }
+    ]
+}
+
+export const FemaleMenus = {
+    menuName: 'Feminino',
+    imgPrefix: 'F',
+    pathName: 'Female',
+    children: [
+        {
+            name: 'Mascaras',
+            value: 179,
+            path: 1
+        },
+        {
+            name: 'Calças',
+            value: 131,
+            path: 4
+        },
+        {
+            name: 'Mochila',
+            value: 84,
+            path: 5
+        },
+        {
+            name: 'Sapatos',
+            value: 99,
+            path: 6
+        },
+        {
+            name: 'NÃO SEI O QUE É',
+            value: 104,
+            path: 7
+        },
+        {
+            name: 'Blusa',
+            value: 200,
+            path: 8
+        },
+        {
+            name: 'PARECE SER COLETE',
+            value: 43,
+            path: 9
+        },
+        {
+            name: 'Jaqueta',
+            value: 346,
+            path: 11
+        },
+        {
+            name: 'Chapeu/P0',
+            value: 147,
+            path: 'p0'
+        },
+        {
+            name: 'Oculos/P1',
+            value: 31,
+            path: 'p1'
+        },
+        {
+            name: 'Brincos/P2',
+            value: 21,
+            path: 'p2'
+        },
+        {
+            name: 'Pulso/P6',
+            value: 28,
+            path: 'p6'
+        },
+        {
+            name: 'Pulso/P7',
+            value: 14,
+            path: 'p7'
+        }
+    ]
+}
